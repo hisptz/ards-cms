e873046e3f766d8ef0af87fe6068605223ebdb30
Andre <amgowano.apm@gmail.com>
2019-10-17 09:51:58 +0300

    fix(sidebar-menu) : Scroll overflow

--- src/app/components/left-sidebar/left-sidebar.component.ts
+++ src/app/components/left-sidebar/left-sidebar.component.ts
@@ -1,4 +1,12 @@
-import {Component, OnInit, Output, EventEmitter, ElementRef, ViewChild, Renderer} from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ElementRef,
+  ViewChild,
+  Renderer
+} from '@angular/core';
 
 @Component({
   selector: 'app-left-sidebar',
@@ -6,12 +14,11 @@ import {Component, OnInit, Output, EventEmitter, ElementRef, ViewChild, Renderer
   styleUrls: ['./left-sidebar.component.css']
 })
 export class LeftSidebarComponent implements OnInit {
-
   sidebarConfiguration: any = {
     normalWidth: '300px',
     largeWidth: '300px',
-    hideWidth: '5px',
-  }
+    hideWidth: '5px'
+  };
 
   @ViewChild('analysis') analysis: ElementRef;
   @ViewChild('documents') documents: ElementRef;
@@ -24,8 +31,7 @@ export class LeftSidebarComponent implements OnInit {
   isMaxSize: boolean = false;
   isMinSize: boolean = false;
 
-  constructor(private renderer: Renderer) {
-  }
+  constructor(private renderer: Renderer) {}
 
   ngOnInit() {
     this.currentWidth = this.sidebarConfiguration.normalWidth;
@@ -56,7 +62,6 @@ export class LeftSidebarComponent implements OnInit {
         this.currentWidth = this.sidebarConfiguration.hideWidth;
         this.hideMenu = true;
       }
-
     }
 
     this.onWidthChange.emit(this.currentWidth);
@@ -65,17 +70,26 @@ export class LeftSidebarComponent implements OnInit {
   getArrowLeftWidth(currentWidth) {
     const width = parseInt(currentWidth.slice(0, -2));
     const newWidth = width > 40 ? width - 40 : 5;
-    return newWidth + 'px'
+    return newWidth + 'px';
   }
 
   clickAccordon(menu, event) {
     event.preventDefault();
-    const action = new MouseEvent('click', {bubbles: true});
+    const action = new MouseEvent('click', { bubbles: true });
     if (event.target.localName !== 'a') {
       this.renderer.invokeElementMethod(
-        this[menu].nativeElement, 'dispatchEvent', [action]);
+        this[menu].nativeElement,
+        'dispatchEvent',
+        [action]
+      );
     }
   }
 
-
+  checkEdge($event) {
+    var sidebar = document.getElementById('sidebar');
+    // console.log(sidebar.scrollTop, sidebar.scrollHeight);
+    if (sidebar.scrollTop >= sidebar.offsetHeight / 2) {
+      sidebar.scrollTop = 20;
+    }
+  }
 }
